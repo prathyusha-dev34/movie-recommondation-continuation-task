4a9fb5dd2796b2480c4b34a9eaf1aa5fc8407fcb
Prathyusha-dev34 <prathyushak795@gmail.com>
2026-07-09 12:34:37 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "http://localhost:8000",
+    "https://movie-recommondation-continuation-task.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
